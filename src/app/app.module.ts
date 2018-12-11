c61d4ac95ae4a8fa7e2d9f5fc57df34ed91a08f8
vaibhav rangarajan <rvaibhav1793@gmail.com>
2018-12-11 00:21:54 -0500

    Updated heroku changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -57,7 +57,7 @@ export function provideConfig() {
     ResultSectionComponent,
     AdminComponent,
     PostAdComponent,
-    FileUploadTestComponent,
+    // FileUploadTestComponent,
     FileSelectDirective,
     AdDetailsComponent,
     MyAdsComponent,
